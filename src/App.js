fa08cff22a3a07426307c8a56123230d8d2e6246
16684814105 <402150786@qq.com>
2023-12-12 11:05:42 +0800

    update it https

--- src/App.js
+++ src/App.js
@@ -251,7 +251,7 @@ function MovieDetails({ watched, selectedId, onMovieClick, onMovieWatched }) {
     const getMovieDetail = async () => {
       setIsLoading(true);
       const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
       );
       const data = await res.json();
       setMovie(data);
